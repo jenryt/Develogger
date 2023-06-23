0899252fad486b631a8407fdfa5ceaa05a5144f3
Jen Ryt <jenyjlin@gmail.com>
2023-06-23 03:22:56 -0700

    routes and controllers setup and updates

--- routes/apiRoutes/commentRoutes/index.js
+++ routes/apiRoutes/commentRoutes/index.js
@@ -1,3 +1,22 @@
 const router = require("express").Router();
-const bcrypt = require("bcrypt");
-const { User, Post, Comment } = require("../../../models");
+const {
+  getAllComment,
+  newComment,
+  updateComment,
+  deleteComment,
+} = require("../../../controllers/commentRoutes");
+const loggedinAuth = require("../../../utils/loggedinAuth");
+
+// GET all comments (no particular users)
+router.get("/", loggedinAuth, getAllComment);
+
+// POST to create new comment and assign it to the user
+router.post("/new", loggedinAuth, newComment);
+
+// PUT to update a comment
+router.put("/:id", loggedinAuth, updateComment);
+
+// DELETE to delete a comment
+router.delete(":id", loggedinAuth, deleteComment);
+
+module.exports = router;
